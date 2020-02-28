73c2f8f8f03208f15d6a18aaa0403f1e2d89d988
zhs007 <sssxueren@gmail.com>
2020-02-28 12:29:53 +0800

    - Updated spark

---
+++ spark/bashmaster.sh
@@ -0,0 +1,1 @@
+docker exec -it spark-master bash
--- spark/startmaster.sh
+++ spark/startmaster.sh
@@ -3,5 +3,5 @@ docker rm spark-master
 docker run --name spark-master \
     -h spark-master \
     -e ENABLE_INIT_DAEMON=false \
-    -d bde2020/spark-master:2.3.0-hadoop2.7 \
+    -d bde2020/spark-master:2.4.5-hadoop2.7 \
     -v $PWD/app:/app
