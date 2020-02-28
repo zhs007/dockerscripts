5c70881504b183f8e7e8d4c8e68cb619ed4f6f0a
zhs007 <sssxueren@gmail.com>
2020-02-28 12:38:02 +0800

    - Updated spark

--- spark/startmaster.sh
+++ spark/startmaster.sh
@@ -2,6 +2,6 @@ docker stop spark-master
 docker rm spark-master
 docker run --name spark-master \
     -h spark-master \
+    -v $PWD/app:/app \
     -e ENABLE_INIT_DAEMON=false \
-    -d bde2020/spark-master:2.4.5-hadoop2.7 \
-    -v $PWD/app:/app
+    -d bde2020/spark-master:2.4.5-hadoop2.7
