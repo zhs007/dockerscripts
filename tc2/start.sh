0c33f1da6d82b3a215db7be87575d442464cca4e
zhs007 <sssxueren@gmail.com>
2020-04-10 13:45:37 +0800

    - Updated tc2

--- tc2/start.sh
+++ tc2/start.sh
@@ -1,7 +1,9 @@
+mkdir output
 docker container stop tc2serv
 docker container rm tc2serv
 docker run -d \
     -p 3777:3777 \
     --name tc2serv \
     -v $PWD/cfg:/app/tr2/cfg \
+    -v $PWD/output:/app/tr2/output \
     zerrozhao/tradingcore2
