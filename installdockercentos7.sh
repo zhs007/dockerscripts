1e7f18a8b80b7f9f2e0db8fbbf2fbbae7028a80a
zhs007 <sssxueren@gmail.com>
2020-04-10 13:11:18 +0800

    - Added installdockercentos7.sh

---
+++ installdockercentos7.sh
@@ -0,0 +1,18 @@
+yum remove -y docker \
+              docker-client \
+              docker-client-latest \
+              docker-common \
+              docker-latest \
+              docker-latest-logrotate \
+              docker-logrotate \
+              docker-engine
+
+yum install -y yum-utils
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install -y docker-ce docker-ce-cli containerd.io
+
+systemctl start docker
