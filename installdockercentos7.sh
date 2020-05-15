30dfa5b4bee2ce46151cdf74b85415dc872a227b
zhs007 <sssxueren@gmail.com>
2020-05-15 19:12:41 +0800

    - Added chatbot.charles

--- installdockercentos7.sh
+++ installdockercentos7.sh
@@ -16,3 +16,6 @@ yum-config-manager \
 yum install -y docker-ce docker-ce-cli containerd.io
 
 systemctl start docker
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
