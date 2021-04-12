80f556fa780e2e130fb15131441bb2d6e6dd6050
zhs007 <zerrozhao@gmail.com>
2021-04-12 16:14:43 +0800

    added docker.registry

---
+++ docker.registry/start.sh
@@ -0,0 +1,11 @@
+mkdir registry
+docker run -d \
+  --restart=always \
+  --name registry \
+  -v $PWD/registry:/var/lib/registry \
+  -v /etc/letsencrypt:/etc/letsencrypt \
+  -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
+  -e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/letsencrypt/live/dockerhub.heyalgo.io/fullchain.pem \
+  -e REGISTRY_HTTP_TLS_KEY=/etc/letsencrypt/live/dockerhub.heyalgo.io/privkey.pem \
+  -p 5001:443 \  
+  registry:2
