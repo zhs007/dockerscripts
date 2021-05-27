fb470d7d8b20d0b5b1f3e946d9bd6d0a12150191
zhs007 <sssxueren@gmail.com>
2021-05-27 23:12:05 +0800

    upd

--- trc2/start.sh
+++ trc2/start.sh
@@ -3,7 +3,6 @@ mkdir logs
 docker container stop trc2serv
 docker container rm trc2serv
 docker run -d \
-    -p 3777:3777 \
     --restart=always \
     --name trc2serv \
     -v $PWD/cfg:/app/tr2/cfg \
